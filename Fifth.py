278589335a780ed6e8dda01653d24474e551b57d
Splashygnu <pachecomoradavidalejandro@gmail.com>
2022-09-25 17:21:58 -0500

    Fifth poitn

---
+++ Fifth.py
@@ -0,0 +1,35 @@
+from matplotlib import pyplot
+
+def FirstFunction(x):#First Function.
+        return (20-(2/3)*x)
+# def SecondFunction(x):#Second Function
+#     return (1/2)
+
+# def ThirdFunction(x):#Second Function
+#     return (x)
+if __name__ == '__main__':
+    x = range(-60, 60) #Values in X taken for the plane
+    # Graph both functions.
+    pyplot.plot(x, [FirstFunction(i) for i in x])
+    # pyplot.plot(x, [SecondFunction(i) for i in x])
+    # pyplot.plot(x, [ThirdFunction(i) for i in x])
+
+        
+    # Establish the axies colors.
+    pyplot.axhline(0, color="black")
+    pyplot.axvline(0, color="black")
+
+    #Filling the area
+    pyplot.fill_between([60,-60],[-20,60],-100, color='green', alpha=.2)
+    # pyplot.fill_between([-6,6],[-6,6],10, color='blue', alpha=.2)
+    # pyplot.fill_between([6,-6],[-9,15],10, color='red', alpha=.5)
+    
+
+    # Limit the axies values
+    pyplot.xlim(0, 100)
+    pyplot.ylim(0, 100)
+
+    # Save the plane as PNG.
+    pyplot.savefig("output.png")
+        
+    pyplot.show()
